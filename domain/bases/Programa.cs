7f4be71609c1df5c36386048ec1b272f16da9529
Nelson Vásquez <nvasquez@aseinfo.com.sv>
2024-08-27 12:45:36 -0600

    Modificando configuración para Programa

--- domain/bases/Programa.cs
+++ domain/bases/Programa.cs
@@ -12,69 +12,82 @@ public partial class Programa
     /// <summary>
     /// Código de programa de onboarding
     /// </summary>
-    public int Codigo { get; set; }
+    public int Codigo { get; set; } // pro_codigo
 
     /// <summary>
     /// Código de plantilla programa
     /// </summary>
-    public int PlantillaProgramaCodigo { get; set; }
+    public int PlantillaProgramaCodigo { get; set; } // pro_codppr
 
     /// <summary>
     /// Nombre del programa de onboarding
     /// </summary>
-    public string Nombre { get; set; }
+    public string Nombre { get; set; } // pro_nombre
 
     /// <summary>
     /// Descripción del programa de onboarding
     /// </summary>
-    public string Descripcion { get; set; }
+    public string Descripcion { get; set; } // pro_descripcion
 
     /// <summary>
     /// Objetivos del programa de onboarding
     /// </summary>
-    public string Objetivo { get; set; }
+    public string Objetivo { get; set; } // pro_objetivo
 
     /// <summary>
-    /// Estado del programa &apos;Planificado&apos; o &apos;En Ejecución&apos; o &apos;Finalizado&apos;
+    /// Estado del programa 'Planificado' o 'En Ejecución' o 'Finalizado'
     /// </summary>
-    public string Estado { get; set; }
+    public string Estado { get; set; } // pro_estado
 
     /// <summary>
     /// Duración estimada del programa
     /// </summary>
-    public int Duracion { get; set; }
+    public int Duracion { get; set; } // pro_duracion
 
     /// <summary>
     /// Unidad de medida de la duración estimada del programa
     /// </summary>
-    public string UnidadDuracion { get; set; }
+    public string UnidadDuracion { get; set; } // pro_unidad_duracion
 
     /// <summary>
     /// Data de los campos adicionales
     /// </summary>
-    public string RawPropertyBagData { get; set; }
+    public string RawPropertyBagData { get; set; } // pro_property_bag_data
 
     /// <summary>
     /// Usuario que creo el registro
     /// </summary>
-    public string UsuarioGrabacion { get; set; }
+    public string UsuarioGrabacion { get; set; } // pro_usuario_grabacion
 
     /// <summary>
     /// Fecha en que se creo el registro
     /// </summary>
-    public DateTime? FechaGrabacion { get; set; }
+    public DateTime? FechaGrabacion { get; set; } // pro_fecha_grabacion
 
     /// <summary>
     /// Usuario que modificó por última vez el registro
     /// </summary>
-    public string UsuarioModificacion { get; set; }
+    public string UsuarioModificacion { get; set; } // pro_usuario_modificacion
 
     /// <summary>
     /// Fecha de la última modificacion del registro
     /// </summary>
-    public DateTime? FechaModificacion { get; set; }
+    public DateTime? FechaModificacion { get; set; } // pro_fecha_modificacion
 
-    public virtual ICollection<ContratacionPrograma> ContratacionesProgramas { get; set; } = new List<ContratacionPrograma>();
+    // Foreign keys
 
-    public virtual PlantillaPrograma PlantillaPrograma { get; set; }
+    /// <summary>
+    /// Parent PlantillaPrograma pointed by [pro_programas].([pro_codppr]) (FK_obdppr_obdpro)
+    /// </summary>
+    [XmlIgnore, JsonIgnore]
+    public virtual PlantillaPrograma PlantillaPrograma { get; set; } // FK_obdppr_obdpro
+
+    // Children collections
+
+    /// <summary>
+    /// Child Contrataciones where [cpr_contrataciones_programa].[cpr_codpro] point to this entity (FK_obdpro_obdcpr)
+    /// </summary>
+    [XmlIgnore, JsonIgnore]
+    public virtual ICollection<ContratacionPrograma> Contrataciones { get; set; } = new List<ContratacionPrograma>(); // FK_obdpro_obdcpr
+
 }
--- persistence/configurations/ProgramaConfiguration.cs
+++ persistence/configurations/ProgramaConfiguration.cs
@@ -20,25 +20,25 @@ namespace onboarding.persistence.configurations
 
         public void Configure(EntityTypeBuilder<Programa> builder)
         {
-            builder.ToTable("pro_programa", _schema);
+            builder.ToTable("pro_programas", _schema);
             builder.HasKey(e => e.Codigo);
 
             builder.Property(e => e.Codigo).HasColumnName("pro_codigo");
+            builder.Property(e => e.PlantillaProgramaCodigo).HasColumnName("pro_codppr");
             builder.Property(e => e.Nombre).HasColumnName("pro_nombre").HasMaxLength(250).IsUnicode(false);
-            builder.Property(e => e.Descripcion).HasColumnName("pro_descripcion").HasMaxLength(500).IsUnicode(false);
-            builder.Property(e => e.FechaInicio).HasColumnName("pro_fecha_inicio").HasColumnType("datetime");
-            builder.Property(e => e.FechaFin).HasColumnName("pro_fecha_fin").HasColumnType("datetime");
-            builder.Property(e => e.FechaGrabacion).HasColumnName("pro_fecha_grabacion").HasColumnType("datetime");
-            builder.Property(e => e.FechaModificacion).HasColumnName("pro_fecha_modificacion").HasColumnType("datetime");
-            builder.Property(e => e.RawPropertyBagData).HasColumnName("pro_property_bag_data").HasColumnType("xml");
+            builder.Property(e => e.Descripcion).HasColumnName("pro_descripcion").HasMaxLength(4000).IsUnicode(false);
+            builder.Property(e => e.Objetivo).HasColumnName("pro_objetivo").HasMaxLength(4000).IsUnicode(false);
+            builder.Property(e => e.Estado).HasColumnName("pro_estado").HasMaxLength(10).IsUnicode(false);
+            builder.Property(e => e.Duracion).HasColumnName("pro_duracion");
+            builder.Property(e => e.UnidadDuracion).HasColumnName("pro_unidad_duracion").HasMaxLength(50).IsUnicode(false);
+            builder.Property(e => e.RawPropertyBagData).HasColumnName("pro_property_bag_data");
             builder.Property(e => e.UsuarioGrabacion).HasColumnName("pro_usuario_grabacion").HasMaxLength(50).IsUnicode(false);
+            builder.Property(e => e.FechaGrabacion).HasColumnName("pro_fecha_grabacion");
             builder.Property(e => e.UsuarioModificacion).HasColumnName("pro_usuario_modificacion").HasMaxLength(50).IsUnicode(false);
+            builder.Property(e => e.FechaModificacion).HasColumnName("pro_fecha_modificacion");
 
-            builder.HasMany(d => d.ActividadesProgramas)
-                .WithOne(p => p.Programa)
-                .HasForeignKey(d => d.ProgramaCodigo)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_obdpro_obdacp");
+            // Foreign keys
+            builder.HasOne(d => d.PlantillaPrograma).WithMany(p => p.Programas).HasForeignKey(d => d.PlantillaProgramaCodigo).OnDelete(DeleteBehavior.NoAction); // FK_obdppr_obdpro
         }
     }
 }

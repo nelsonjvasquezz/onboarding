9b11a0a6aed3d87b8b11afafb43edec41cd481d7
Nelson Vásquez <nvasquez@aseinfo.com.sv>
2024-08-27 09:11:19 -0600

    Se agregan comentarios de columnas en propiedades. Se agrega summary según convención para las navigation properties.

--- domain/bases/NotificacionActividadPrograma.cs
+++ domain/bases/NotificacionActividadPrograma.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
 
 namespace onboarding.data.bases;
 
@@ -11,59 +13,69 @@ public partial class NotificacionActividadPrograma
     /// <summary>
     /// Código de registro de la configuración de notificaciones de la actividad del programa
     /// </summary>
-    public int Codigo { get; set; }
+    public int Codigo { get; set; } // nap_codigo
 
     /// <summary>
     /// Código de la Actividad
     /// </summary>
-    public int ActividadProgramaCodigo { get; set; }
+    public int ActividadProgramaCodigo { get; set; } // nap_codacp
 
     /// <summary>
     /// Código de Evento Notificable
     /// </summary>
-    public int EventoNotificableCodigo { get; set; }
+    public int EventoNotificableCodigo { get; set; } // nap_codeno
 
     /// <summary>
     /// Desplazamiento en días de la fecha en que ocurre el evento para el envio de la notificación
     /// </summary>
-    public int OffsetDias { get; set; }
+    public int OffsetDias { get; set; } // nap_offset_dias
 
     /// <summary>
     /// Destinatario de la notificación: Contratado | JefeInmediato | ResponsableActividad | ResponsableRRHH
     /// </summary>
-    public string TipoDestinatario { get; set; } = null;
+    public string TipoDestinatario { get; set; } = null; // nap_tipo_destinatario
 
     /// <summary>
     /// Asunto de la notificación
     /// </summary>
-    public string Subject { get; set; }
+    public string Subject { get; set; } // nap_subject
 
     /// <summary>
     /// Cuerpo Markdown de la notificación
     /// </summary>
-    public string Body { get; set; }
+    public string Body { get; set; } // nap_body
 
     /// <summary>
     /// Fecha en que sucedió el evento notificable (está null mientras no se ha dado)
     /// </summary>
-    public DateTime? FechaEvento { get; set; }
+    public DateTime? FechaEvento { get; set; } // nap_fecha_evento
 
     /// <summary>
     /// Fecha y hora de cuando se notificó
     /// </summary>
-    public DateTime? FechaNotificacion { get; set; }
+    public DateTime? FechaNotificacion { get; set; } // nap_fecha_notificacion
 
     /// <summary>
-    /// Estado de la notificación (NULL | &apos;Pendiente&apos; | &apos;Enviada&apos; | &apos;Error&apos;)
+    /// Estado de la notificación (NULL | 'Pendiente' | 'Enviada' | 'Error')
     /// </summary>
-    public string Estado { get; set; }
+    public string Estado { get; set; } // nap_estado
 
     /// <summary>
-    /// Mensaje o excepción cuando el estado es &apos;Error&apos;
+    /// Mensaje o excepción cuando el estado es 'Error'
     /// </summary>
-    public string MensajeError { get; set; }
+    public string MensajeError { get; set; } // nap_mensaje_error
 
-    public virtual ActividadPrograma ActividadPrograma { get; set; }
+    // Foreign keys
 
-    public virtual EventoNotificable EventoNotificable { get; set; }
+    /// <summary>
+    /// Parent ActividadPrograma pointed by [nap_notif_actividad_programa].[(ActividadProgramaCodigo)] (FK_obdacp_obdnap)
+    /// </summary>
+    [XmlIgnore, JsonIgnore]
+    public virtual ActividadPrograma ActividadPrograma { get; set; } // FK_obdacp_obdnap
+
+    /// <summary>
+    /// Parent EventoNotificable pointed by [nap_notif_actividad_programa].[(EventoNotificableCodigo)] (FK_obdeno_obdnap)
+    /// </summary>
+    [XmlIgnore, JsonIgnore]
+    public virtual EventoNotificable EventoNotificable { get; set; } // FK_obdeno_obdnap
 }
--- persistence/configurations/NotificacionActividadProgramaConfiguration.cs
+++ persistence/configurations/NotificacionActividadProgramaConfiguration.cs
@@ -28,8 +28,8 @@ namespace onboarding.persistence.configurations
             builder.Property(e => e.ActividadProgramaCodigo).HasColumnName("nap_codacp");
             builder.Property(e => e.EventoNotificableCodigo).HasColumnName("nap_codeno");
             builder.Property(e => e.Estado).HasColumnName("nap_estado").HasMaxLength(10).IsUnicode(false);
-            builder.Property(e => e.FechaEvento).HasColumnName("nap_fecha_evento").HasColumnType("datetime");
-            builder.Property(e => e.FechaNotificacion).HasColumnName("nap_fecha_notificacion").HasColumnType("datetime");
+            builder.Property(e => e.FechaEvento).HasColumnName("nap_fecha_evento");
+            builder.Property(e => e.FechaNotificacion).HasColumnName("nap_fecha_notificacion");
             builder.Property(e => e.MensajeError).HasColumnName("nap_mensaje_error").HasMaxLength(4000).IsUnicode(false);
             builder.Property(e => e.OffsetDias).HasColumnName("nap_offset_dias");
             builder.Property(e => e.Subject).HasColumnName("nap_subject").HasMaxLength(250).IsUnicode(false);

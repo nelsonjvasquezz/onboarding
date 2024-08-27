2c60513b7b5728fb84ffff766b12051f69ca2a7a
Nelson Vásquez <nvasquez@aseinfo.com.sv>
2024-08-27 14:15:53 -0600

    Modificando configuración para TipoEvaluacion

--- domain/bases/TipoEvaluacion.cs
+++ domain/bases/TipoEvaluacion.cs
@@ -12,59 +12,69 @@ public partial class TipoEvaluacion
     /// <summary>
     /// Código del tipo de evaluación
     /// </summary>
-    public int Codigo { get; set; }
+    public int Codigo { get; set; } // tev_codigo
 
     /// <summary>
     /// Código del Grupo Corporativo
     /// </summary>
-    public int GrupoCorporativoCodigo { get; set; }
+    public int GrupoCorporativoCodigo { get; set; } // tev_codgrc
 
     /// <summary>
     /// Nombre del tipo de evaluación
     /// </summary>
-    public string Nombre { get; set; }
+    public string Nombre { get; set; } // tev_nombre
 
     /// <summary>
     /// Descripción del tipo de evaluación
     /// </summary>
-    public string Descripcion { get; set; }
+    public string Descripcion { get; set; } // tev_descripcion
 
     /// <summary>
     /// Determina si el tipo de evaluación utiliza un formulario dinámico
     /// </summary>
-    public bool UtilizaFormularioDinamico { get; set; }
+    public bool UtilizaFormularioDinamico { get; set; } // tev_utiliza_fdi
 
     /// <summary>
     /// Código de formulario dinámico que se utiliza para evaluar una actividad cuando si se utiliza
     /// </summary>
-    public int? FormularioDinamicoCodigo { get; set; }
+    public int? FormularioDinamicoCodigo { get; set; } // tev_codfdi_evaluacion
 
     /// <summary>
     /// Data de los campos adicionales
     /// </summary>
-    public string RawPropertyBagData { get; set; }
+    public string RawPropertyBagData { get; set; } // tev_property_bag_data
 
     /// <summary>
     /// Usuario que creo el registro
     /// </summary>
-    public string UsuarioGrabacion { get; set; }
+    public string UsuarioGrabacion { get; set; } // tev_usuario_grabacion
 
     /// <summary>
     /// Fecha en que se creo el registro
     /// </summary>
-    public DateTime? FechaGrabacion { get; set; }
+    public DateTime? FechaGrabacion { get; set; } // tev_fecha_grabacion
 
     /// <summary>
     /// Usuario que modificó por última vez el registro
     /// </summary>
-    public string UsuarioModificacion { get; set; }
+    public string UsuarioModificacion { get; set; } // tev_usuario_modificacion
 
     /// <summary>
     /// Fecha de la última modificacion del registro
     /// </summary>
-    public DateTime? FechaModificacion { get; set; }
+    public DateTime? FechaModificacion { get; set; } // tev_fecha_modificacion
 
-    public virtual ICollection<ActividadPrograma> ActividadesProgramas { get; set; } = new List<ActividadPrograma>();
+    // Children collections
 
-    public virtual ICollection<PlantillaActividad> PlantillasDeActividades { get; set; } = new List<PlantillaActividad>();
+    /// <summary>
+    /// Child Actividades where [acp_actividades_programa].[acp_codtev] point to this entity (FK_obdte_obdacp)
+    /// </summary>
+    [XmlIgnore, JsonIgnore]
+    public virtual ICollection<ActividadPrograma> Actividades { get; set; } = new List<ActividadPrograma>(); // FK_obdte_obdacp
+
+    /// <summary>
+    /// Child PlantillaActividades where [pac_plant_actividades].[pac_codtac] point to this entity (FK_obdtev_obdpac)
+    /// </summary>
+    [XmlIgnore, JsonIgnore]
+    public virtual ICollection<PlantillaActividad> PlantillasDeActividades { get; set; } = new List<PlantillaActividad>(); // FK_obdtev_obdpac
 }
--- persistence/configurations/TipoEvaluacionConfiguration.cs
+++ persistence/configurations/TipoEvaluacionConfiguration.cs
@@ -20,23 +20,20 @@ namespace onboarding.persistence.configurations
 
         public void Configure(EntityTypeBuilder<TipoEvaluacion> builder)
         {
-            builder.ToTable("tev_tipo_evaluacion", _schema);
+            builder.ToTable("tev_tipos_evaluacion", _schema);
             builder.HasKey(e => e.Codigo);
 
             builder.Property(e => e.Codigo).HasColumnName("tev_codigo");
+            builder.Property(e => e.GrupoCorporativoCodigo).HasColumnName("tev_codgrc");
             builder.Property(e => e.Nombre).HasColumnName("tev_nombre").HasMaxLength(100).IsUnicode(false);
             builder.Property(e => e.Descripcion).HasColumnName("tev_descripcion").HasMaxLength(500).IsUnicode(false);
-            builder.Property(e => e.FechaGrabacion).HasColumnName("tev_fecha_grabacion").HasColumnType("datetime");
-            builder.Property(e => e.FechaModificacion).HasColumnName("tev_fecha_modificacion").HasColumnType("datetime");
-            builder.Property(e => e.RawPropertyBagData).HasColumnName("tev_property_bag_data").HasColumnType("xml");
+            builder.Property(e => e.UtilizaFormularioDinamico).HasColumnName("tev_utiliza_fdi");
+            builder.Property(e => e.FormularioDinamicoCodigo).HasColumnName("tev_codfdi_evaluacion");
+            builder.Property(e => e.RawPropertyBagData).HasColumnName("tev_property_bag_data");
             builder.Property(e => e.UsuarioGrabacion).HasColumnName("tev_usuario_grabacion").HasMaxLength(50).IsUnicode(false);
+            builder.Property(e => e.FechaGrabacion).HasColumnName("tev_fecha_grabacion");
             builder.Property(e => e.UsuarioModificacion).HasColumnName("tev_usuario_modificacion").HasMaxLength(50).IsUnicode(false);
-
-            builder.HasMany(d => d.Evaluaciones)
-                .WithOne(p => p.TipoEvaluacion)
-                .HasForeignKey(d => d.TipoEvaluacionCodigo)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_obdtev_obdevl");
+            builder.Property(e => e.FechaModificacion).HasColumnName("tev_fecha_modificacion");
         }
     }
 }

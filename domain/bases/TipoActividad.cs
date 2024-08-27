87d36d8e540f9ef2e7aad52646b5b56422ec536c
Nelson Vásquez <nvasquez@aseinfo.com.sv>
2024-08-27 12:57:42 -0600

    Modificando configuración para TipoActividad

--- domain/bases/TipoActividad.cs
+++ domain/bases/TipoActividad.cs
@@ -12,84 +12,94 @@ public partial class TipoActividad
     /// <summary>
     /// Código de tipo de actividad
     /// </summary>
-    public int Codigo { get; set; }
+    public int Codigo { get; set; } // tac_codigo
 
     /// <summary>
     /// Código de grupo corporativo
     /// </summary>
-    public int GrupoCorporativoCodigo { get; set; }
+    public int GrupoCorporativoCodigo { get; set; } // tac_codgrc
 
     /// <summary>
     /// Nombre del tipo de actividad
     /// </summary>
-    public string Nombre { get; set; }
+    public string Nombre { get; set; } // tac_nombre
 
     /// <summary>
     /// Descripción del tipo de actividad
     /// </summary>
-    public string Descripcion { get; set; }
+    public string Descripcion { get; set; } // tac_descripcion
 
     /// <summary>
     /// ¿Requiere Notificación luego de que finalice? (contratado, jefe, RRHH y al responsable de la actividad si está definido)
     /// </summary>
-    public bool RequiereNotificacion { get; set; }
+    public bool RequiereNotificacion { get; set; } // tac_req_notificacion
 
     /// <summary>
     /// ¿Requiere Recordatorio previo a que se inicie? (contratado, jefe, RRHH y al responsable de la actividad si está definido)
     /// </summary>
-    public bool RequiereRecordatorio { get; set; }
+    public bool RequiereRecordatorio { get; set; } // tac_req_recordatorio
 
     /// <summary>
     /// ¿Requiere Evaluación del responsable? (contratado, jefe, RRHH y al responsable de la actividad si está definido)
     /// </summary>
-    public bool RequiereEvaluacion { get; set; }
+    public bool RequiereEvaluacion { get; set; } // tac_req_evaluacion
 
     /// <summary>
     /// ¿Requiere Adjuntar documentos?
     /// </summary>
-    public bool RequiereDocumentos { get; set; }
+    public bool RequiereDocumentos { get; set; } // tac_req_documentos
 
     /// <summary>
     /// ¿Requiere un responsable asignado?
     /// </summary>
-    public bool RequiereResponsable { get; set; }
+    public bool RequiereResponsable { get; set; } // tac_req_responsable
 
     /// <summary>
     /// Duración estimada de la actividad
     /// </summary>
-    public int DuracionEstimada { get; set; }
+    public int DuracionEstimada { get; set; } // tac_duracion_estimada
 
     /// <summary>
     /// Unidad de medida para la duración estimada
     /// </summary>
-    public string UnidadDuracion { get; set; }
+    public string UnidadDuracion { get; set; } // tac_unidad_duracion
 
     /// <summary>
     /// Data de los campos adicionales
     /// </summary>
-    public string RawPropertyBagData { get; set; }
+    public string RawPropertyBagData { get; set; } // tac_property_bag_data
 
     /// <summary>
     /// Usuario que creo el registro
     /// </summary>
-    public string UsuarioGrabacion { get; set; }
+    public string UsuarioGrabacion { get; set; } // tac_usuario_grabacion
 
     /// <summary>
     /// Fecha en que se creo el registro
     /// </summary>
-    public DateTime? FechaGrabacion { get; set; }
+    public DateTime? FechaGrabacion { get; set; } // tac_fecha_grabacion
 
     /// <summary>
     /// Usuario que modificó por última vez el registro
     /// </summary>
-    public string UsuarioModificacion { get; set; }
+    public string UsuarioModificacion { get; set; } // tac_usuario_modificacion
 
     /// <summary>
     /// Fecha de la última modificacion del registro
     /// </summary>
-    public DateTime? FechaModificacion { get; set; }
+    public DateTime? FechaModificacion { get; set; } // tac_fecha_modificacion
 
-    public virtual ICollection<ActividadPrograma> ActividadesProgramas { get; set; } = new List<ActividadPrograma>();
+    // Children collections
 
+    /// <summary>
+    /// Child Actividades where [acp_actividades_programa].[acp_codtac] point to this entity (FK_obdtac_obdacp)
+    /// </summary>
+    [XmlIgnore, JsonIgnore]
+    public virtual ICollection<ActividadPrograma> Actividades { get; set; } = new List<ActividadPrograma>();
+
+    /// <summary>
+    /// Child PlantillaActividades where [pac_plant_actividades].[pac_codtac] point to this entity (FK_obdtac_obdpac)
+    /// </summary>
+    [XmlIgnore, JsonIgnore]
     public virtual ICollection<PlantillaActividad> PlantillasDeActividades { get; set; } = new List<PlantillaActividad>();
 }
--- persistence/configurations/TipoActividadConfiguration.cs
+++ persistence/configurations/TipoActividadConfiguration.cs
@@ -20,23 +20,25 @@ namespace onboarding.persistence.configurations
 
         public void Configure(EntityTypeBuilder<TipoActividad> builder)
         {
-            builder.ToTable("tac_tipo_actividad", _schema);
+            builder.ToTable("tac_tipos_actividad", _schema);
             builder.HasKey(e => e.Codigo);
 
             builder.Property(e => e.Codigo).HasColumnName("tac_codigo");
+            builder.Property(e => e.GrupoCorporativoCodigo).HasColumnName("tac_codgrc");
             builder.Property(e => e.Nombre).HasColumnName("tac_nombre").HasMaxLength(100).IsUnicode(false);
             builder.Property(e => e.Descripcion).HasColumnName("tac_descripcion").HasMaxLength(500).IsUnicode(false);
-            builder.Property(e => e.FechaGrabacion).HasColumnName("tac_fecha_grabacion").HasColumnType("datetime");
-            builder.Property(e => e.FechaModificacion).HasColumnName("tac_fecha_modificacion").HasColumnType("datetime");
-            builder.Property(e => e.RawPropertyBagData).HasColumnName("tac_property_bag_data").HasColumnType("xml");
+            builder.Property(e => e.RequiereNotificacion).HasColumnName("tac_req_notificacion");
+            builder.Property(e => e.RequiereRecordatorio).HasColumnName("tac_req_recordatorio");
+            builder.Property(e => e.RequiereEvaluacion).HasColumnName("tac_req_evaluacion");
+            builder.Property(e => e.RequiereDocumentos).HasColumnName("tac_req_documentos");
+            builder.Property(e => e.RequiereResponsable).HasColumnName("tac_req_responsable");
+            builder.Property(e => e.DuracionEstimada).HasColumnName("tac_duracion_estimada");
+            builder.Property(e => e.UnidadDuracion).HasColumnName("tac_unidad_duracion").HasMaxLength(50).IsUnicode(false);
+            builder.Property(e => e.RawPropertyBagData).HasColumnName("tac_property_bag_data");
             builder.Property(e => e.UsuarioGrabacion).HasColumnName("tac_usuario_grabacion").HasMaxLength(50).IsUnicode(false);
+            builder.Property(e => e.FechaGrabacion).HasColumnName("tac_fecha_grabacion");
             builder.Property(e => e.UsuarioModificacion).HasColumnName("tac_usuario_modificacion").HasMaxLength(50).IsUnicode(false);
-
-            builder.HasMany(d => d.ActividadesProgramas)
-                .WithOne(p => p.TipoActividad)
-                .HasForeignKey(d => d.TipoActividadCodigo)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_obdtac_obdacp");
+            builder.Property(e => e.FechaModificacion).HasColumnName("tac_fecha_modificacion");
         }
     }
 }

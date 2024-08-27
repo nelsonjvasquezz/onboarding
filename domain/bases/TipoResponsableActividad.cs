d8108550b5412afa1faa2373e2c4971908f8fea8
Nelson Vásquez <nvasquez@aseinfo.com.sv>
2024-08-27 14:31:57 -0600

    Modificando configuración para TipoResponsableActividad

--- domain/bases/TipoResponsableActividad.cs
+++ domain/bases/TipoResponsableActividad.cs
@@ -1,5 +1,4 @@
-﻿using onboarding.data.bases;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace onboarding.data;
@@ -13,84 +12,94 @@ public partial class TipoResponsableActividad
     /// <summary>
     /// Código de Tipo de Resonsable de Actividad
     /// </summary>
-    public int Codigo { get; set; }
+    public int Codigo { get; set; } // tra_codigo
 
     /// <summary>
     /// Código de Grupo Corporativo
     /// </summary>
-    public int GrupoCorporativoCodigo { get; set; }
+    public int GrupoCorporativoCodigo { get; set; } // tra_codgrc
 
     /// <summary>
     /// Nombre del Tipo de Resonsable de Actividad
     /// </summary>
-    public string Nombre { get; set; }
+    public string Nombre { get; set; } // tra_nombre
 
     /// <summary>
     /// Descripción del Tipo de Resonsable de Actividad
     /// </summary>
-    public string Descripcion { get; set; }
+    public string Descripcion { get; set; } // tra_descripcion
 
     /// <summary>
     /// ¿Requiere la selección de un empleado?
     /// </summary>
-    public bool RequiereEmpleo { get; set; }
+    public bool RequiereEmpleo { get; set; } // tra_requiere_empleo
 
     /// <summary>
     /// Código de la lista de valores que permite seleccionar un empleo
     /// </summary>
-    public string EmpleoValueListCodigo { get; set; }
+    public string EmpleoValueListCodigo { get; set; } // tra_codvli_empleo
 
     /// <summary>
     /// ¿Corresponde con una institución externa?
     /// </summary>
-    public bool EsInstExterna { get; set; }
+    public bool EsInstExterna { get; set; } // tra_es_inst_externa
 
     /// <summary>
     /// Nombre de una persona contacto, cuando es una institución externa
     /// </summary>
-    public string ContactoExterno { get; set; }
+    public string ContactoExterno { get; set; } // tra_contacto_externo
 
     /// <summary>
     /// ¿Corresponde con una persona externa?
     /// </summary>
-    public bool EsPersonaExterna { get; set; }
+    public bool EsPersonaExterna { get; set; } // tra_es_persona_externa
 
     /// <summary>
     /// Email de notificaciones si es una persona o institución externa
     /// </summary>
-    public string EmailExterno { get; set; }
+    public string EmailExterno { get; set; } // tra_email_externo
 
     /// <summary>
     /// Teléfono de contacto cuando es una persona o institución externa
     /// </summary>
-    public string TelefonoExterno { get; set; }
+    public string TelefonoExterno { get; set; } // tra_telefono_externo
 
     /// <summary>
     /// Data de los campos adicionales
     /// </summary>
-    public string RawPropertyBagData { get; set; }
+    public string RawPropertyBagData { get; set; } // tra_property_bag_data
 
     /// <summary>
     /// Usuario que creo el registro
     /// </summary>
-    public string UsuarioGrabacion { get; set; }
+    public string UsuarioGrabacion { get; set; } // tra_usuario_grabacion
 
     /// <summary>
     /// Fecha en que se creo el registro
     /// </summary>
-    public DateTime? FechaGrabacion { get; set; }
+    public DateTime? FechaGrabacion { get; set; } // tra_fecha_grabacion
 
     /// <summary>
     /// Usuario que modificó por última vez el registro
     /// </summary>
-    public string UsuarioModificacion { get; set; }
+    public string UsuarioModificacion { get; set; } // tra_usuario_modificacion
 
     /// <summary>
     /// Fecha de la última modificacion del registro
     /// </summary>
-    public DateTime? FechaModificacion { get; set; }
+    public DateTime? FechaModificacion { get; set; } // tra_fecha_modificacion
 
-    public virtual ICollection<ActividadPrograma> ActividadesProgramas { get; set; } = new List<ActividadPrograma>();
+    // Children collections
 
-    public virtual ICollection<PlantillaActividad> PlantillasDeActividades { get; set; } = new List<PlantillaActividad>();
+    /// <summary>
+    /// Child Actividades where [acp_actividades_programa].[acp_codtra] point to this entity (FK_obdtra_obdacp)
+    /// </summary>
+    [XmlIgnore, JsonIgnore]
+    public virtual ICollection<ActividadPrograma> Actividades { get; set; } = new List<ActividadPrograma>(); // FK_obdtra_obdacp
+
+    /// <summary>
+    /// Child PlantillasDeActividades where [pac_plant_actividades].[pac_codtra] point to this entity (FK_obdtra_obdpac)
+    /// </summary>
+    [XmlIgnore, JsonIgnore]
+    public virtual ICollection<PlantillaActividad> PlantillasDeActividades { get; set; } = new List<PlantillaActividad>(); // FK_obdtra_obdpac
 }
--- persistence/configurations/TipoResponsableActividadConfiguration.cs
+++ persistence/configurations/TipoResponsableActividadConfiguration.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using onboarding.data.bases;
+using onboarding.data;
 
 namespace onboarding.persistence.configurations
 {
@@ -20,23 +20,25 @@ namespace onboarding.persistence.configurations
 
         public void Configure(EntityTypeBuilder<TipoResponsableActividad> builder)
         {
-            builder.ToTable("tra_tipo_responsable_actividad", _schema);
+            builder.ToTable("tra_tipos_respon_actividad", _schema);
             builder.HasKey(e => e.Codigo);
 
             builder.Property(e => e.Codigo).HasColumnName("tra_codigo");
+            builder.Property(e => e.GrupoCorporativoCodigo).HasColumnName("tra_codgrc");
             builder.Property(e => e.Nombre).HasColumnName("tra_nombre").HasMaxLength(100).IsUnicode(false);
             builder.Property(e => e.Descripcion).HasColumnName("tra_descripcion").HasMaxLength(500).IsUnicode(false);
-            builder.Property(e => e.FechaGrabacion).HasColumnName("tra_fecha_grabacion").HasColumnType("datetime");
-            builder.Property(e => e.FechaModificacion).HasColumnName("tra_fecha_modificacion").HasColumnType("datetime");
-            builder.Property(e => e.RawPropertyBagData).HasColumnName("tra_property_bag_data").HasColumnType("xml");
+            builder.Property(e => e.RequiereEmpleo).HasColumnName("tra_requiere_empleo");
+            builder.Property(e => e.EmpleoValueListCodigo).HasColumnName("tra_codvli_empleo");
+            builder.Property(e => e.EsInstExterna).HasColumnName("tra_es_inst_externa");
+            builder.Property(e => e.ContactoExterno).HasColumnName("tra_contacto_externo").HasMaxLength(250).IsUnicode(false);
+            builder.Property(e => e.EsPersonaExterna).HasColumnName("tra_es_persona_externa");
+            builder.Property(e => e.EmailExterno).HasColumnName("tra_email_externo").HasMaxLength(100).IsUnicode(false);
+            builder.Property(e => e.TelefonoExterno).HasColumnName("tra_telefono_externo").HasMaxLength(50).IsUnicode(false);
+            builder.Property(e => e.RawPropertyBagData).HasColumnName("tra_property_bag_data");
             builder.Property(e => e.UsuarioGrabacion).HasColumnName("tra_usuario_grabacion").HasMaxLength(50).IsUnicode(false);
+            builder.Property(e => e.FechaGrabacion).HasColumnName("tra_fecha_grabacion");
             builder.Property(e => e.UsuarioModificacion).HasColumnName("tra_usuario_modificacion").HasMaxLength(50).IsUnicode(false);
-
-            builder.HasMany(d => d.ActividadesProgramas)
-                .WithOne(p => p.TipoResponsableActividad)
-                .HasForeignKey(d => d.TipoResponsableActividadCodigo)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_obdtra_obdacp");
+            builder.Property(e => e.FechaModificacion).HasColumnName("tra_fecha_modificacion");
         }
     }
 }

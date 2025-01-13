4cf971e069920639a5330c0cf7864bd64bfecdbf
Sara Isabel García Herrera <sgarcia@aseinfo.com.sv>
2025-01-13 17:03:27 -0600

    Se agregaron los scripts con los datos de prueba para el esquema de onboarding en oracle

---
+++ Database/Data/Oracle/OBD_ENO_EVENTOS_NOTIFICABLES.sql
@@ -0,0 +1,10 @@
+INSERT INTO OBD_ENO_EVENTOS_NOTIFICABLES (ENO_CODIGO, ENO_NOMBRE, ENO_DESCRIPCION_LOC_KEY) VALUES (1, 'Inicio del Programa', '{DescripcionInicioPrograma, Onboarding}');
+INSERT INTO OBD_ENO_EVENTOS_NOTIFICABLES (ENO_CODIGO, ENO_NOMBRE, ENO_DESCRIPCION_LOC_KEY) VALUES (2, 'DescripcionPrimeraEvaluacionDesempenio', '{DescripcionPrimeraEvaluacionDesempenio, Onboarding}');
+INSERT INTO OBD_ENO_EVENTOS_NOTIFICABLES (ENO_CODIGO, ENO_NOMBRE, ENO_DESCRIPCION_LOC_KEY) VALUES (3, 'Asignación de Mentor', '{DescripcionAsignacionMentor, Onboarding}');
+INSERT INTO OBD_ENO_EVENTOS_NOTIFICABLES (ENO_CODIGO, ENO_NOMBRE, ENO_DESCRIPCION_LOC_KEY) VALUES (4, 'Reunión de Retroalimentación', '{DescripcionReunionRetroalimentacion, Onboarding}');
+INSERT INTO OBD_ENO_EVENTOS_NOTIFICABLES (ENO_CODIGO, ENO_NOMBRE, ENO_DESCRIPCION_LOC_KEY) VALUES (5, 'Plan de Desarrollo Actualizado', '{DescripcionActualizacionPlan, Onboarding}');
+INSERT INTO OBD_ENO_EVENTOS_NOTIFICABLES (ENO_CODIGO, ENO_NOMBRE, ENO_DESCRIPCION_LOC_KEY) VALUES (6, 'Programa de Onboarding Completado', '{DescripcionFinPrograma, Onboarding}');
+INSERT INTO OBD_ENO_EVENTOS_NOTIFICABLES (ENO_CODIGO, ENO_NOMBRE, ENO_DESCRIPCION_LOC_KEY) VALUES (7, 'Firma de Contrato Pendiente', '{DescripcionFirmaContrato, Onboarding}');
+INSERT INTO OBD_ENO_EVENTOS_NOTIFICABLES (ENO_CODIGO, ENO_NOMBRE, ENO_DESCRIPCION_LOC_KEY) VALUES (8, 'Capacitación Técnica Pendiente', '{DescripcionCapacitacionTecnica, Onboarding}');
+INSERT INTO OBD_ENO_EVENTOS_NOTIFICABLES (ENO_CODIGO, ENO_NOMBRE, ENO_DESCRIPCION_LOC_KEY) VALUES (9, 'Revisión de Políticas', '{DescripcionRevisionPoliticas, Onboarding}');
+INSERT INTO OBD_ENO_EVENTOS_NOTIFICABLES (ENO_CODIGO, ENO_NOMBRE, ENO_DESCRIPCION_LOC_KEY) VALUES (10, 'Configuración de herramientas de trabajo', '{DescripcionConfiguracionHerramientas, Onboarding}');
